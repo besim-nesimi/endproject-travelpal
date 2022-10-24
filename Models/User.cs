fbdf2f4f2fc233dad973e88ff116fd0e2fd331b6
bestronaut <nesimibesim@outlook.com>
2022-10-24 12:42:21 +0200

    Version 0.1 - MainWindow UI
    MainWindow UI ready for work. Classes UserManager, Admin and User created.
    
    An admin has been hardcoded.
    
    Buttons have no logic yet.

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slutproj_TravelPal.Models
+{
+    internal class User
+    {
+    }
+}
